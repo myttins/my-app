76e92f10c50d9205e36e175772f1132936342c85
Kevin Li <kevin.li128@gmail.com>
2024-02-18 17:02:09 -0800

    added reload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [input, setInput] = useState('')
-  const [echo, setEcho] = useState('')
+  const [input, setInput] = useState("");
+  const [echo, setEcho] = useState("");
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem("echoedString");
+    if (storedValue) {
+      setEcho(storedValue);
+    }
+  }, []);
 
   const handleSubmit = () => {
-    setEcho(input)
-  }
-
+    localStorage.setItem("echoedString", input);
+    window.location.reload();
+  };
 
   return (
     <div className="App">
-      <input onChange={(e) => setInput(e.target.value)}/>
+      <input onChange={(e) => setInput(e.target.value)} />
       <button onClick={handleSubmit}>Submit</button>
       <p>{echo}</p>
     </div>
   );
-}
+};
 
 export default App;
